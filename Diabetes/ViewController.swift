8abe748006ddfedc3a7e2150e913dc59078980a2
Aluno Mack <alunomack@10-44-48-135.0200.mack.br.iosda.org>
2019-10-08 10:47:38 -0300

    teste

--- Diabetes/ViewController.swift
+++ Diabetes/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func SearshButton(_ sender: Any) {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self as? UISearchBarDelegate
+        present(searchController,animated: true,completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
